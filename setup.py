3e8f9648d1694329c1541e47100523abb6723a6d
F3Mozk1_ <memozki@bk.ru>
2025-02-25 03:17:28 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name="Mechgram",
+    version="1.0",
+    description="Unique library for creating Telegram bots with fluent interface ",
+    author="MEMozki",
+    author_email="memerz@list.ru",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)

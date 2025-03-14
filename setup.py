509bdbe999ddd097354ffe23c1598268316d8b1f
F3Mozk1_ <memozki@bk.ru>
2025-03-15 02:20:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 setup(
     name="MEchgram",
-    version="1.3.2",
+    version="1.4.0",
     description="Telegram bot api via requests",
+    url="https://github.com/MEMozki/MEchgram",
     author="MEMozki",
     author_email="memerz@list.ru",
+    license="MIT",
     packages=find_packages(),
     install_requires=[
         "requests"
